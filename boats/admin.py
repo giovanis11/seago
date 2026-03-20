e73eb5127c141acc88c6a27dffdee47a57272626
Giovanis <nikolaos.giovanis@acg.edu>
2026-03-20 02:40:58 +0200

    add admin registration for all models

--- boats/admin.py
+++ boats/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Boat, BoatCategory, BoatImage, WishList
 
-# Register your models here.
+admin.site.register(Boat)
+admin.site.register(BoatCategory)
+admin.site.register(BoatImage)
+admin.site.register(WishList)
+
